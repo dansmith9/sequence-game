c85fe9570b4be9f3864c66e0cd8d2c950d2ee901
dansmith9 <dansmith9@users.noreply.github.com>
2015-08-30 00:53:20 +0100

    Room created and lives added

--- game.py
+++ game.py
@@ -7,14 +7,22 @@ import random
 
 mc = Minecraft.create()
 
-puzzleRoomStart = Vec3(0,35,0)
-boardstart = Vec3(0,35,0)
-boardend = Vec3(8,35,8)
-blue = Vec3(7,36,1)
-yellow = Vec3(7,36,3)
-red = Vec3(7,36,5)
-green = Vec3(7,36,7)
-startPos = Vec3(4,36,4)
+puzRoomStart = Vec3(0,0,0)
+
+puzRoomLength = 12
+puzRoomWidth = 12
+puzRoomHeight = 8
+
+puzRoomFloorStart = Vec3(2,0,2)
+puzRoomFloorEnd = Vec3(puzRoomLength-2,1,puzRoomWidth-2)
+
+print(puzRoomFloorEnd)
+
+blue = Vec3(3,2,4)
+yellow = Vec3(5,2,4)
+red = Vec3(7,2,4)
+green = Vec3(9,2,4)
+startPos = Vec3(6,2,10)
 
 difficulty=1
 sequence = []
@@ -23,18 +31,39 @@ blocklist = [blue,yellow,red,green]
 correct = 0
 currentBlock = 0
 
-def clear():
-    mc.setBlocks(boardstart.x-10, boardstart.y-10, boardstart.z-10,boardend.x+10, boardend.y+10, boardend.z+10,block.AIR)
+lives = 4
 
+def clear():
+    mc.setBlocks(puzRoomStart.x-10, puzRoomStart.y-10, puzRoomStart.z-10,puzRoomStart.x+30, puzRoomStart.y+30, puzRoomStart.z+30,block.AIR)
 
 def setup():
-    mc.setBlocks(boardstart.x, boardstart.y, boardstart.z,boardend.x, boardend.y, boardend.z,block.DIAMOND_BLOCK)
-    mc.setBlocks(boardstart.x, boardstart.y-1, boardstart.z,boardend.x, boardend.y-20, boardend.z,block.AIR)
-    mc.setBlocks(boardstart.x, boardstart.y-20, boardstart.z,boardend.x, boardend.y-20, boardend.z,block.LAVA_FLOWING)
+    ##create puzzle room outer TNT skin
+    mc.setBlocks(puzRoomStart.x,puzRoomStart.y-20,puzRoomStart.z,puzRoomStart.x+puzRoomLength,puzRoomStart.y+puzRoomHeight,puzRoomStart.z+puzRoomWidth,block.TNT,1)
+    ##patterned inner layer of wall
+    pattern=[block.WOOL,block.WOOL,block.WOOL.id,11,block.WOOL,block.WOOL.id,4,block.WOOL,block.WOOL.id,14,block.WOOL,block.WOOL.id,13,block.WOOL,block.WOOL]
+    pattern=[0,0,11,0,4,0,14,0,13,0,0]
+    for i in range(0,len(pattern)):
+        mc.setBlocks(puzRoomStart.x+i+1,puzRoomStart.y+1,puzRoomStart.z+1,puzRoomStart.x+i+1,puzRoomStart.y+puzRoomHeight,puzRoomStart.z+puzRoomWidth-1,block.WOOL.id,pattern[i])
+
+
+    ##make centre of room hollow
+    mc.setBlocks(puzRoomStart.x+2,puzRoomStart.y+2,puzRoomStart.z+2,puzRoomStart.x+puzRoomLength-2,puzRoomStart.y+puzRoomHeight,puzRoomStart.z+puzRoomWidth-2,block.AIR)
+
+    ## make pit hollow
+    mc.setBlocks(puzRoomStart.x+2,puzRoomStart.y-1,puzRoomStart.z+2,puzRoomStart.x+puzRoomLength-2,puzRoomStart.y-19,puzRoomStart.z+puzRoomWidth-2,block.AIR)
+
+    ##make lava pit
+    mc.setBlocks(puzRoomStart.x,puzRoomStart.y-19,puzRoomStart.z,puzRoomStart.x+puzRoomLength,puzRoomStart.y-19,puzRoomStart.z+puzRoomWidth,block.LAVA)
+    
+    ##make glass roof
+    mc.setBlocks(puzRoomStart.x,puzRoomStart.y+puzRoomHeight,puzRoomStart.z,puzRoomStart.x+puzRoomLength,puzRoomStart.y+puzRoomHeight,puzRoomStart.z+puzRoomWidth,block.GLASS)
+    
+    ##place coloured wool blocks
     mc.setBlock(blue,block.WOOL.id,11)
     mc.setBlock(yellow,block.WOOL.id,4)
     mc.setBlock(red,block.WOOL.id,14)
     mc.setBlock(green,block.WOOL.id,13)
+    ##place player
     mc.player.setPos(startPos)
     time.sleep(2)
     mc.setBlock(blue,block.WOOL.id,15)
@@ -42,9 +71,12 @@ def setup():
     mc.setBlock(red,block.WOOL.id,15)
     mc.setBlock(green,block.WOOL.id,15)
 
+    mc.player.setPos(startPos)
+
 def createSequence():
     for i in range(0,difficulty):
         sequence.append(blocklist[random.randint(0,len(blocklist)-1)])
+    print("CS Seq:",sequence)
 
 def showSequence():
     for i in sequence:
@@ -81,13 +113,23 @@ def showSequence():
 ##        time.sleep(1)
 ##        explorerhat.light.green.off()
 
+clear()
+setup()
+
 while True:
-    clear()
-    setup()
+
+    mc.postToChat("Please wait...")
+
+    time.sleep(5)
+    
+    lives=4
+    difficulty=1
+    sequence=[]
+
+    mc.postToChat("Lives: "+str(lives))
+    
     createSequence()
-    print("current block:",currentBlock)
     print(sequence)
-    print("Next sequence:",sequence[currentBlock])
 
     mc.postToChat("Watch the sequence!")
 
@@ -95,38 +137,52 @@ while True:
 
     showSequence()
 
-    
     #mc.postToChat("Press 1 on the Explorer for a hint!")
 
     #explorerhat.touch.one.pressed(hint)
 
-    while correct == 0:
+    while lives!=0:
+            
         for event in mc.events.pollBlockHits():
             print("Event pos:",event.pos)
             print("Next sequence:",sequence[currentBlock])
-            if event.pos.x == sequence[currentBlock].x and event.pos.y == sequence[currentBlock].y and event.pos.z == sequence[currentBlock].z:
-                mc.setBlock(event.pos,block.GOLD_BLOCK)
-                time.sleep(1)
-                mc.setBlock(event.pos,block.WOOL.id,15)
-                currentBlock += 1
-                print(currentBlock)
-            else:
-                mc.setBlocks(boardstart,boardend,block.AIR)
-                mc.postToChat("That is wrong, you muppet!")
-                time.sleep(3)
-                currentBlock=0
-                setup()
-                mc.setBlock(startPos.x, startPos.y, startPos.z,block.WATER_STATIONARY)
-                time.sleep(0.5)
-                mc.setBlock(startPos.x, startPos.y, startPos.z,block.AIR)
+            if (event.pos.x == blue.x and event.pos.y == blue.y and event.pos.z == blue.z) or (event.pos.x == yellow.x and event.pos.y == yellow.y and event.pos.z == yellow.z) or (event.pos.x == red.x and event.pos.y == red.y and event.pos.z == red.z) or (event.pos.x == green.x and event.pos.y == green.y and event.pos.z == green.z):
+                if event.pos.x == sequence[currentBlock].x and event.pos.y == sequence[currentBlock].y and event.pos.z == sequence[currentBlock].z:
+                    mc.setBlock(event.pos,block.GOLD_BLOCK)
+                    time.sleep(1)
+                    mc.setBlock(event.pos,block.WOOL.id,15)
+                    currentBlock += 1
+                else:
+                    mc.setBlocks(puzRoomFloorStart,puzRoomFloorEnd,block.AIR)
+                    mc.postToChat("That is wrong, you muppet!")
+                    lives-=1
+                    mc.postToChat("Lives left: "+str(lives))
+                    time.sleep(3)
+                    print("Current block",currentBlock)
+                    currentBlock=0
+                    print("Current block",currentBlock)
+                    setup()
+                    mc.setBlock(startPos.x, startPos.y, startPos.z,block.WATER_STATIONARY)
+                    time.sleep(0.5)
+                    mc.setBlock(startPos.x, startPos.y, startPos.z,block.AIR)
+
         if currentBlock == len(sequence):
             correct = 1
-                
-    mc.postToChat("That is correct! Well done you legend!")
-    mc.postToChat("Now lets make it harder...")
-    correct=0
-    currentBlock=0
-    difficulty+=1
-    sequence = []
+            mc.postToChat("That is correct! Well done you legend!")
+            mc.postToChat("Now lets make it harder...")
+            currentBlock=0
+            difficulty+=1
+            sequence = []
+            clear()
+            setup()
+            createSequence()
+            print(sequence)
 
-print(sequence)
+            mc.postToChat("Watch the sequence!")
+
+            time.sleep(5)
+
+            showSequence()
+
+                
+
