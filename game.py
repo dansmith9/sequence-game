237e00729eb396e8b0f103e96d29da1393ebae86
dansmith9 <dansmith9@users.noreply.github.com>
2015-08-31 22:33:09 +0100

    added UH celebrations

--- game.py
+++ game.py
@@ -8,7 +8,6 @@ from math import trunc
 import picamera
 import unicornhat as UH
 from threading import Thread
-#import explorerhat
 
 mc = Minecraft.create()
 
@@ -45,6 +44,14 @@ Pixels = [
     [[(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]],
     [[(0, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (132, 0, 0), (0, 0, 0)], [(0, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (132, 0, 0), (0, 0, 0), (0, 0, 0)]],
     ]
+
+UHBlue = [[(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)], [(0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89), (0, 0, 89)]]
+UHYellow = [[(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)], [(132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0), (132, 132, 0)]]
+UHRed = [[(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)], [(80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0), (80, 0, 0)]]
+UHGreen = [[(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)], [(0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0), (0, 81, 0)]]
+UHOff = [[(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)], [(0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0)]]
+
+sparkle = 0
 
 def clear():
     mc.setBlocks(puzRoomStart.x-10, puzRoomStart.y-10, puzRoomStart.z-10,puzRoomStart.x+30, puzRoomStart.y+30, puzRoomStart.z+30,block.AIR)
@@ -83,7 +90,7 @@ def setup():
     mc.setBlock(yellow,block.WOOL.id,15)
     mc.setBlock(red,block.WOOL.id,15)
     mc.setBlock(green,block.WOOL.id,15)
-
+    
     mc.player.setPos(startPos)
 
 def createSequence():
@@ -95,17 +102,22 @@ def showSequence():
     for i in sequence:
         if i == blue:
             mc.setBlock(i,block.WOOL.id,11)
+            updateUH(UHBlue)
         elif i == yellow:
             mc.setBlock(i,block.WOOL.id,4)
+            updateUH(UHYellow)
         elif i == red:
             mc.setBlock(i,block.WOOL.id,14)
+            updateUH(UHRed)
         elif i == green:
             mc.setBlock(i,block.WOOL.id,13)
+            updateUH(UHGreen)
         time.sleep(1)
         mc.setBlock(blue,block.WOOL.id,15)
         mc.setBlock(yellow,block.WOOL.id,15)
         mc.setBlock(red,block.WOOL.id,15)
         mc.setBlock(green,block.WOOL.id,15)
+        updateUH(UHOff)
         time.sleep(1)
 
 def correctSequence():
@@ -133,11 +145,14 @@ def photoBooth():
     mc.setBlocks(puzRoomStart.x+(puzRoomLength//2),puzRoomStart.y+2,puzRoomStart.z+1,puzRoomStart.x+((puzRoomLength//2)),puzRoomStart.y+(puzRoomHeight//2)-1,puzRoomStart.z,block.AIR)
     mc.setBlock(puzRoomStart.x+((puzRoomLength//2)),puzRoomStart.y+(puzRoomHeight//2)-1,puzRoomStart.z-1,block.GLASS)
     cameraTrigger = Vec3(puzRoomStart.x+((puzRoomLength//2)),puzRoomStart.y+2,puzRoomStart.z)
+    mc.postToChat("Walk all the way into the booth!")
     while True:
         pos = mc.player.getPos()
         if trunc(pos.x) == cameraTrigger.x and trunc(pos.y) == cameraTrigger.y and trunc(pos.z) == cameraTrigger.z:
             mc.setBlocks(pos.x,pos.y,pos.z+1,pos.x,pos.y+1,pos.z,block.GLASS)
             mc.postToChat("Look at the camera!")
+            sparkle=0
+            print("Sparkle:",sparkle)
             for i in range(len(Pixels),0,-1):
                 print("Seconds:",i-1)
                 t = Thread(target=updateUH, args=(Pixels[i-1],))
@@ -152,34 +167,26 @@ def updateUH(pixels):
     UH.set_pixels(pixels)
     UH.show()
 
+def UHSparkle():
+    for i in range(3000):
+        x = random.randint(0, 7)
+        y = random.randint(0, 7)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        UH.set_pixel(x, y, r, g, b)
+        UH.show()
+
 def capture_frame():
     with picamera.PiCamera() as cam:
-        cam.capture('/home/pi/Images/winner'+str(datetime.day)+'.jpg')
+        filename = time.strftime("%Y%m%d-%H%M%S")
+        cam.capture('/home/pi/Images/'+ time.strftime("%Y%m%d-%H%M%S") +'.jpg')
 
-##def hint(channel,event):
-##    if sequence[currentBlock] == blue:
-##        explorerhat.light.blue.on()
-##        time.sleep(1)
-##        explorerhat.light.blue.off()
-##    elif sequence[currentBlock] == yellow:
-##        explorerhat.light.yellow.on()
-##        time.sleep(1)
-##        explorerhat.light.yellow.off()
-##    elif sequence[currentBlock] == red:
-##        explorerhat.light.red.on()
-##        time.sleep(1)
-##        explorerhat.light.red.off()
-##    elif sequence[currentBlock] == green:
-##        explorerhat.light.green.on()
-##        time.sleep(1)
-##        explorerhat.light.green.off()
-
+capture_frame()
 clear()
-setup()
-photoBooth()
 
 while True:
-
+    setup()
     mc.postToChat("Please wait...")
 
     time.sleep(5)
@@ -193,20 +200,18 @@ while True:
     createSequence()
     print(sequence)
 
-    mc.postToChat("Watch the sequence!")
+    mc.postToChat("Ready? Watch the sequence!")
 
     time.sleep(5)
 
     showSequence()
 
-    #mc.postToChat("Press 1 on the Explorer for a hint!")
-
-    #explorerhat.touch.one.pressed(hint)
-
     while lives!=0:
             
         for event in mc.events.pollBlockHits():
             print("Event pos:",event.pos)
+            print("Len sequence:",len(sequence))
+            print("Current block:", currentBlock)
             print("Next sequence:",sequence[currentBlock])
             if (event.pos.x == blue.x and event.pos.y == blue.y and event.pos.z == blue.z) or (event.pos.x == yellow.x and event.pos.y == yellow.y and event.pos.z == yellow.z) or (event.pos.x == red.x and event.pos.y == red.y and event.pos.z == red.z) or (event.pos.x == green.x and event.pos.y == green.y and event.pos.z == green.z):
                 if event.pos.x == sequence[currentBlock].x and event.pos.y == sequence[currentBlock].y and event.pos.z == sequence[currentBlock].z:
@@ -216,13 +221,15 @@ while True:
                     currentBlock += 1
                 else:
                     mc.setBlocks(puzRoomFloorStart,puzRoomFloorEnd,block.AIR)
-                    mc.postToChat("That is wrong, you muppet!")
+                    mc.postToChat("That is wrong! What a muppet!")
                     lives-=1
-                    mc.postToChat("Lives left: "+str(lives))
-                    time.sleep(3)
-                    print("Current block",currentBlock)
+                    if lives==0:
+                        mc.postToChat("*** GAME OVER ***")
+                        time.sleep(5)
+                    else:
+                        mc.postToChat("Lives left: "+str(lives))
+                        time.sleep(3)
                     currentBlock=0
-                    print("Current block",currentBlock)
                     setup()
                     mc.setBlock(startPos.x, startPos.y, startPos.z,block.WATER_STATIONARY)
                     time.sleep(0.5)
@@ -230,15 +237,21 @@ while True:
 
         if currentBlock == len(sequence):
             correct = 1
-            mc.postToChat("That is correct! Well done you legend!")
+            mc.postToChat("That is correct! You are a legend!")
             correctSequence()
-            mc.postToChat("Now lets make it harder...")
+            time.sleep(3)
             currentBlock=0
             difficulty+=1
             sequence = []
-            if difficulty == 10:
-                print("")#camera
+            if difficulty > 10:
+                mc.postToChat("YOU HAVE BEATEN THE CUBE(S)!")
+                sparkle=1
+                t = Thread(target=UHSparkle)
+                t.start()
+                photoBooth()
+                lives=0
             else:
+                mc.postToChat("Now lets make it harder...")
                 clear()
                 setup()
                 createSequence()
